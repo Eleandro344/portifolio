e59048f6369997f9d368bd3a6219792407053a9b
ELEADNRO344 <eleandromartins23@hotmail.com>
2024-09-11 19:24:07 -0300

    novoteste2

--- portifolio/settings.py
+++ portifolio/settings.py
@@ -1,10 +1,10 @@
 
-
+import pymysql
+pymysql.install_as_MySQLdb()
 import os 
 from dotenv import load_dotenv
 import sys
 
-import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
